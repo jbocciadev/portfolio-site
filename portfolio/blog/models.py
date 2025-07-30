f16f261b76129820a473936d1bcf233c8c4361e5
jbocciadev <jboccia@gmail.com>
2025-07-30 16:04:29 +0100

    Implement blog app with Article model and first test article.

--- portfolio/blog/models.py
+++ portfolio/blog/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Article(models.Model):
+    title = models.CharField(max_length=100)
+    excerpt = models.CharField(max_length=300)
+    content = models.TextField(null=True)
+
+def __str__(self):
+    return f"Article {self.id}" 
